2f46b5728d75d1b8d9854a149ac87f3f80775fc2
Natashi <32347635+Natashi@users.noreply.github.com>
2025-03-08 15:36:30 +0700

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,24 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, provideAppInitializer, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, withComponentInputBinding, withInMemoryScrolling } from '@angular/router';
 
-import { routes } from './app.routes';
+import { appRoutes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+	providers: [
+		provideZoneChangeDetection({ eventCoalescing: true }),
+		
+		provideAnimations(),
+		provideHttpClient(),
+		provideRouter(
+			appRoutes,
+			withInMemoryScrolling({ scrollPositionRestoration: 'enabled' }),
+			withComponentInputBinding(),
+		),
+		
+		provideAppInitializer(() => {
+			
+		}),
+	]
 };
